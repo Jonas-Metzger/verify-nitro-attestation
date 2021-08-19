41c5179dac783759133c105aa5bc29945a08f74e
Jonas-Metzger <metzgerj@stanford.edu>
2021-08-18 22:10:10 -0700

    removed all that was holy

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,155 @@
-use aws_nitro_enclaves_cose as cose;
-use nsm_io::AttestationDoc;
-use cose::CoseSign1;
-use openssl::{x509, stack};
+//use aws_nitro_enclaves_cose as cose;
+//use cose::CoseSign1;
+//use openssl::{x509, stack};
+
+
+use std::collections::{BTreeMap, BTreeSet};
+use std::io::Error as IoError;
+use std::result;
+use serde;
+use serde::{Deserialize, Serialize};
+use serde_bytes::ByteBuf;
+use serde_cbor::error::Error as CborError;
+use serde_cbor::{from_slice, to_vec};
+
+#[derive(Debug)]
+/// Possible error types return from this library.
+pub enum Error {
+    /// An IO error of type `std::io::Error`
+    Io(IoError),
+    /// A CBOR ser/de error of type `serde_cbor::error::Error`.
+    Cbor(CborError),
+}
+
+/// Result type return nsm-io::Error on failure.
+pub type Result<T> = result::Result<T, Error>;
+
+impl From<IoError> for Error {
+    fn from(error: IoError) -> Self {
+        Error::Io(error)
+    }
+}
+
+impl From<CborError> for Error {
+    fn from(error: CborError) -> Self {
+        Error::Cbor(error)
+    }
+}
+
+
+/// The digest implementation used by a NitroSecureModule
+#[repr(C)]
+#[derive(Debug, Serialize, Deserialize, Copy, Clone, PartialEq)]
+pub enum Digest {
+    /// SHA256
+    SHA256,
+    /// SHA384
+    SHA384,
+    /// SHA512
+    SHA512,
+}
+
+/// An attestation response.  This is also used for sealing
+/// data.
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
+pub struct AttestationDoc {
+    /// Issuing NSM ID
+    pub module_id: String,
+
+    /// The digest function used for calculating the register values
+    /// Can be: "SHA256" | "SHA512"
+    pub digest: Digest,
+
+    /// UTC time when document was created expressed as seconds since Unix Epoch
+    pub timestamp: u64,
+
+    /// Map of all locked PCRs at the moment the attestation document was generated
+    pub pcrs: BTreeMap<usize, ByteBuf>,
+
+    /// The infrastucture certificate used to sign the document, DER encoded
+    pub certificate: ByteBuf,
+    /// Issuing CA bundle for infrastructure certificate
+    pub cabundle: Vec<ByteBuf>,
+
+    /// An optional DER-encoded key the attestation consumer can use to encrypt data with
+    pub public_key: Option<ByteBuf>,
+
+    /// Additional signed user data, as defined by protocol.
+    pub user_data: Option<ByteBuf>,
+
+    /// An optional cryptographic nonce provided by the attestation consumer as a proof of
+    /// authenticity.
+    pub nonce: Option<ByteBuf>,
+}
+
+impl AttestationDoc {
+    /// Creates a new AttestationDoc.
+    ///
+    /// # Arguments
+    ///
+    /// * module_id: a String representing the name of the NitroSecureModule
+    /// * digest: nsm_io::Digest that describes what the PlatformConfigurationRegisters
+    ///           contain
+    /// * pcrs: BTreeMap containing the index to PCR value
+    /// * certificate: the serialized certificate that will be used to sign this AttestationDoc
+    /// * cabundle: the serialized set of certificates up to the root of trust certificate that
+    ///             emitted `certificate`
+    /// * user_data: optional user definted data included in the AttestationDoc
+    /// * nonce: optional cryptographic nonce that will be included in the AttestationDoc
+    /// * public_key: optional DER-encoded public key that will be included in the AttestationDoc
+    #[allow(clippy::too_many_arguments)]
+    pub fn new(
+        module_id: String,
+        digest: Digest,
+        timestamp: u64,
+        pcrs: BTreeMap<usize, Vec<u8>>,
+        certificate: Vec<u8>,
+        cabundle: Vec<Vec<u8>>,
+        user_data: Option<Vec<u8>>,
+        nonce: Option<Vec<u8>>,
+        public_key: Option<Vec<u8>>,
+    ) -> Self {
+        let mut pcrs_serialized = BTreeMap::new();
+
+        for (i, pcr) in pcrs.into_iter() {
+            let pcr = ByteBuf::from(pcr);
+            pcrs_serialized.insert(i, pcr);
+        }
+
+        let cabundle_serialized = cabundle.into_iter().map(ByteBuf::from).collect();
+
+        AttestationDoc {
+            module_id,
+            digest,
+            timestamp,
+            pcrs: pcrs_serialized,
+            cabundle: cabundle_serialized,
+            certificate: ByteBuf::from(certificate),
+            user_data: user_data.map(ByteBuf::from),
+            nonce: nonce.map(ByteBuf::from),
+            public_key: public_key.map(ByteBuf::from),
+        }
+    }
+
+    /// Helper function that converts an AttestationDoc structure to its CBOR representation
+    pub fn to_binary(&self) -> Vec<u8> {
+        // This should not fail
+        to_vec(self).unwrap()
+    }
+
+    /// Helper function that parses a CBOR representation of an AttestationDoc and creates the
+    /// structure from it, if possible.
+    pub fn from_binary(bin: &[u8]) -> Result<Self> {
+        from_slice(bin).map_err(Error::Cbor)
+    }
+}
+
 
 fn main() {
-	let cose_doc = CoseSign1::from_bytes(&std::fs::read("data/attestation_doc").unwrap()).unwrap();
-        let payload = cose_doc.get_payload(None).unwrap();
-        let attestation_doc = AttestationDoc::from_binary(&payload).unwrap();
-	let cert = x509::X509::from_der(&attestation_doc.certificate).unwrap();
+	//let cose_doc = CoseSign1::from_bytes(&std::fs::read("data/attestation_doc").unwrap()).unwrap();
+        //let payload = cose_doc.get_payload(None).unwrap();
+        //let attestation_doc = AttestationDoc::from_binary(&payload).unwrap();
+	/*let cert = x509::X509::from_der(&attestation_doc.certificate).unwrap();
 	println!("checking signature...");
 	let signature_valid = cose_doc.verify_signature(&cert.public_key().unwrap()).unwrap();
 	assert!(signature_valid);
@@ -47,5 +189,5 @@ IwLz3/Y=
         let pcr0 = hex::encode(pcr0);
 	println!("PCR0: {:#?}", pcr0);
         let extracted = format!("{}|{}", public_key, pcr0);
-        let _ = std::fs::write("data/extracted.txt", extracted);
+        let _ = std::fs::write("data/extracted.txt", extracted);*/
 }
