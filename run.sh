9d6ea54e5de41aa90ceceba75478ad9fa6b5b76c
Jonas Metzger <metzgerj@stanford.edu>
2022-01-05 16:51:40 +0100

    simplified things

--- run.sh
+++ run.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-wasmer run --enable-all --dir /dev --dir . target/wasm32-wasi/debug/verify_attestation.wasm
-# access to /dev/urandom is required because of https://stackoverflow.com/questions/61235285/why-does-ecc-signature-verification-need-random-numbers-sometimes-taking-a-long
-# fake randomness is fine
+wasmer run --enable-all --mapdir ./dev:/dev --mapdir ./data:./data target/wasm32-wasi/debug/verify_attestation.wasm
